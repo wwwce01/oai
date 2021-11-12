bd79022e30f86f1fe9d4222b3b071b0854b58103
Shweta Shrivastava <s.shrivastava@northeastern.edu>
2021-11-11 21:30:53 -0600

    Fix/workaround to support multiple UEs

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_primitives.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_primitives.c
@@ -149,7 +149,13 @@ void set_dl_dmrs_ports(NR_pdsch_semi_static_t *ps) {
       ps->numDmrsCdmGrpsNoData = 2;
       break;
     default:
-      AssertFatal(1==0,"Number of layers %d\n not supported or not valid\n",ps->nrOfLayers);
+      // Temporary fix for issue seen with multiple OAI UEs
+      // TODO Fix issue causing ps->nrOfLayers to be invalid
+      // AssertFatal(1==0,"Number of layers %d\n not supported or not valid\n",ps->nrOfLayers);
+      LOG_W(MAC,"Number of layers %d\n not supported or not valid. TODO: Check the issue\n",ps->nrOfLayers);
+      ps->nrOfLayers = 1;
+      ps->dmrs_ports_id = 0;
+      ps->numDmrsCdmGrpsNoData = 1;
   }
 }
 
